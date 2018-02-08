66fa006d54ac4fdb152c9373b62c34ae112f8677
Vikrant Arya <vikrantarya21@gmail.com>
2018-02-09 00:20:19 +0530

    Update NaiveBayesianMethod.py

--- NaiveBayesianMethod.py
+++ NaiveBayesianMethod.py
@@ -1,13 +1,19 @@
 import os
 import io
 import numpy
-#Handling tabular data
+#pandas lib use for reading data from tabular form
 from pandas import DataFrame
-#Use this library and function for naive baysian funciton
+#Using sklearn library's function we can calculate naive baysian 
 from sklearn.feature_extraction.text import CountVectorizer
+#NB --> Naive baysian
 from sklearn.naive_bayes import MultinomialNB
 
+#Generator
 def readFiles(path):
+    """OS.walk() generate the file names in a directory tree by walking the tree either top-down or bottom-up.
+    For each directory in the tree rooted at directory top (including top itself),
+    it yields a 3-tuple (dirpath, dirnames, filenames)."""
+    
     for root, dirnames, filenames in os.walk(path):
         for filename in filenames:
             path = os.path.join(root, filename)
@@ -18,13 +24,16 @@ def readFiles(path):
             for line in f:
                 if inBody:
                     lines.append(line)
+                #escape header
                 elif line == '\n':
                     inBody = True
             f.close()
             message = '\n'.join(lines)
+            """https://github.com/coderc/MachineLearningConceptAndCode/blob/master/python/iterable-generators-yield.text"""
             yield path, message
 
 
+#data from datafiles
 def dataFrameFromDirectory(path, classification):
     rows = []
     index = []
@@ -33,8 +42,29 @@ def dataFrameFromDirectory(path, classification):
         index.append(filename)
 
     return DataFrame(rows, index=index)
+#Create table with message and class --> spam/ham
+data = DataFrame({'message': [], 'class': []},index=[])
+data = data.append(dataFrameFromDirectory('/home/vikranth/Downloads/VikrantPersonal/Machine-Learning/DataScience-Python3/emails/spam/', 'spam'))
+data = data.append(dataFrameFromDirectory('/home/vikranth/Downloads/VikrantPersonal/Machine-Learning/DataScience-Python3/emails/ham/', 'ham'))
 
-data = DataFrame({'message': [], 'class': []})
+#validate data 
+data.head()
 
-data = data.append(dataFrameFromDirectory('e:/sundog-consult/Udemy/DataScience/emails/spam', 'spam'))
-data = data.append(dataFrameFromDirectory('e:/sundog-consult/Udemy/DataScience/emails/ham', 'ham'))
+"""Now we will use a CountVectorizer to split up each message into its list of words,"""
+vectorizer = CountVectorizer()
+"""It will change word into number and store its frequency"""
+counts = vectorizer.fit_transform(data['message'].values)
+"""Naive Baysian classifier"""
+classifier = MultinomialNB()
+"""Set target for training set"""
+targets = data['class'].values
+"""use classifier to fit data set"""
+classifier.fit(counts, targets)
+
+"""example"""
+examples = ['Free games now!!!', "Hi mike, how about a game of golf tomorrow?"]
+"""Word count """
+example_counts = vectorizer.transform(examples)
+"""Check test data from trained classifier"""
+predictions = classifier.predict(example_counts)
+predictions
