7437bef668350a3910e9b2ba1c636759d74717ad
Vikrant Arya <vikrantarya21@gmail.com>
2018-02-10 14:34:59 +0530

    Adding decision tree

---
+++ decision-tree.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from sklearn import tree
+
+input_file = "/home/vikranth/Downloads/VikrantPersonal/Machine-Learning/DataScience-Python3/PastHires.csv"
+df = pd.read_csv(input_file, header = 0)
